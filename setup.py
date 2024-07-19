6203517d546b806cc898079db45d8a3c434273ad
D1ng5 <m.becker@dings.software>
2024-07-19 12:43:11 +0200

    nix wichtiges

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ import ssl
 try:
     nltk.download('wordnet')
     nltk.download('omw')  # Dieser Befehl lädt das deutsche WordNet (GermaNet)
-    nltk.download('deu_lexicon')  # Dieser Befehl lädt ein deutsches Lexikon
+    nltk.download('ger_lexicon')  # Dieser Befehl lädt ein deutsches Lexikon
     print("fertig")
 except:
+    print("Ohne SSL...")
     # Umgehen der Zertifikatverifizierung (nicht empfohlen für produktive Umgebungen)
     ssl._create_default_https_context = ssl._create_unverified_context
     nltk.download('wordnet')
     nltk.download('omw')  # Dieser Befehl lädt das deutsche WordNet (GermaNet)
-    nltk.download('deu_lexicon')  # Dieser Befehl lädt ein deutsches Lexikon
+    nltk.download('ger_lexicon')  # Dieser Befehl lädt ein deutsches Lexikon
     print("fertig")
