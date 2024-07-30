a584151096955e98ecbba44a2419fe359de31ed2
MulmVerbot <70117614+MulmVerbot@users.noreply.github.com>
2024-07-31 01:28:42 +0200

    TotDo Liste Alpha 1.1
    sehr viele Bugfixes und verbesserungen.

--- totdo.py
+++ totdo.py
@@ -1,34 +1,40 @@
-import customtkinter as Atk
-import tkinter as tk
-from tkinter import simpledialog, messagebox
+import customtkinter as tk
 import json
 import os
 import time
+Benutzerordner = os.path.expanduser('~')
+tasks_pfad = os.path.join(Benutzerordner, 'CiM', 'Db')
+tasks_pfad_datei = os.path.join(tasks_pfad, 'tasks.json')
 
-TASKS_FILE = 'tasks.json'
+try:
+    if not os.path.exists(tasks_pfad):
+        print("[-INFO-] Der Db Ordner scheint nicht zu existieren. Erstelle ihn nun.")
+        os.mkdir(tasks_pfad)
+        print("[-INFO-] Der Db Ordner wurde erfolgreich erstellt.")
+except Exception as ex_einst:
+    print("[-ERR-] Fehler beim Erstellen des Db Ordners. Fehlercode:", ex_einst)
+TASKS_FILE = tasks_pfad_datei
 
 class TodoApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("TotDo Liste Alpha 1.0")
+        self.root.title("TotDo Liste Alpha 1.1")
         self.Todo_offen = False
-        self.Todo_aufmachen_main_knopp = tk.Button(self.root, text="Todo öffnen", command=self.todo_aufmachen)
-        self.Todo_aufmachen_main_knopp.pack(pady=10)
-        self.Zeit = time.strftime("%H:%M:%S")
         
+        self.Zeit = time.strftime("%H:%M:%S")
+        self.todo_aufmachen()
 
     def todo_aufmachen(self):
         self.Todo_offen = True
-        self.Todo_aufmachen_main_knopp.configure(text="Todo schließen", command=self.todo_zumachen)
-        self.todo_frame = tk.Frame(self.root, width=600, height=380, borderwidth=3)
+        self.todo_frame = tk.CTkFrame(self.root, width=600, height=380)
         self.todo_frame.place(x=100, y=100)
-        self.todo_frame_einz = Atk.CTkScrollableFrame(self.root, width=300, height=220)
+        self.todo_frame_einz = tk.CTkScrollableFrame(self.root, width=300, height=220)
         self.todo_frame_einz.place(x=300, y=100)
-        self.Aufgaben_name_e = Atk.CTkEntry(self.todo_frame)
-        self.Aufgaben_Beschreibung_e = Atk.CTkEntry(self.todo_frame)
+        self.Aufgaben_name_e = tk.CTkEntry(self.todo_frame)
+        self.Aufgaben_Beschreibung_e = tk.CTkEntry(self.todo_frame)
         self.Aufgaben_name_e.place(x=10, y=100)
         self.Aufgaben_Beschreibung_e.place(x=10, y=70)
-        self.Aufgabe_hinzufuegen_Knopp = tk.Button(self.todo_frame, text="Aufgabe erstellen", command=self.create_task_button_vor)
+        self.Aufgabe_hinzufuegen_Knopp = tk.CTkButton(self.todo_frame, text="Aufgabe erstellen", command=self.create_task_button_vor)
         self.Aufgabe_hinzufuegen_Knopp.place(x=10, y=10)
         self.load_tasks()
 
@@ -44,7 +50,6 @@ class TodoApp:
         self.Aufgaben_Beschreibung_e.place_forget()
         self.Aufgabe_hinzufuegen_Knopp.place_forget()
         self.Todo_offen = False
-        self.Todo_aufmachen_main_knopp.configure(text="Todo öffnen", command=self.todo_aufmachen)
 
     def create_task_button_vor(self):
         task_name = self.Aufgaben_name_e.get()
@@ -59,13 +64,35 @@ class TodoApp:
                 self.create_task_button(self.task)
 
     def create_task_button(self, task):
-        button = tk.Button(self.todo_frame_einz, text=task['name'], command=lambda t=task: self.show_task(t))
-        button.pack(padx=10, pady=5)
+        self.button = tk.CTkButton(self.todo_frame_einz, text=task['name'], command=lambda t=task: self.show_task(t))
+        self.button.pack(padx=10, pady=5)
+
+    def l_ja(self):
+        self.delete_task(self.task)
+        self.top_show_f.destroy()
+
+    def l_nein(self):
+        self.top_show_f.destroy()
 
     def show_task(self, task):
-        result = messagebox.askyesno(task['name'], task['description'] + "\n\nMöchten Sie diese Aufgabe löschen?")
-        if result:  # Wenn der Benutzer "Ja" auswählt
-            self.delete_task(task)
+        self.top_show_f = tk.CTkToplevel()
+        self.task = task
+        ja = tk.CTkButton(self.top_show_f, text="Ja", command=self.l_ja)
+        ja.pack(pady=5,padx=5)
+        nein = tk.CTkButton(self.top_show_f, text="Nein", command=self.l_nein)
+        nein.pack(pady=5,padx=5)
+        width = 250
+        height = 100
+        def mittig_fenster(width, height):
+            fenster_breite = root.winfo_screenwidth()
+            fenster_hoehe = root.winfo_screenheight()
+            x = (fenster_breite - width) // 2
+            y = (fenster_hoehe - height) // 2
+
+            # Leg die Position des Fensters fest
+            self.top_show_f.geometry(f"{width}x{height}+{x}+{y}")
+        mittig_fenster(width, height)
+        
 
     def save_task(self, task):
         tasks = self.load_tasks_from_file()
@@ -101,14 +128,14 @@ class TodoApp:
             widget.destroy()
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    root = tk.CTk()
     width = 720
     height = 420
     def mittig_fenster(root, width, height):
         fenster_breite = root.winfo_screenwidth()
-        fenster_höhe = root.winfo_screenheight()
+        fenster_hoehe = root.winfo_screenheight()
         x = (fenster_breite - width) // 2
-        y = (fenster_höhe - height) // 2
+        y = (fenster_hoehe - height) // 2
 
         # Leg die Position des Fensters fest
         root.geometry(f"{width}x{height}+{x}+{y}")
