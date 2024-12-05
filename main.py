6ec5f25d9be1b85f75ec11a0b02516827227900b
MulmVerbot <70117614+MulmVerbot@users.noreply.github.com>
2024-12-05 21:46:20 +0100

    Beta 1.1.2 (11)
    - hab mal verhindert dass random der Speicher volläuft

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ class Listendings:
         self.master = master
         self.Programm_Name = "M.U.L.M" # -> sowas nennt man übrigens ein Apronym, ist einem Akronym sehr ähnlich aber nicht gleich << Danke Du klugscheißer
         self.Programm_Name_lang = "Multifunktionaler Unternehmens-Logbuch-Manager"
-        self.Version = "Beta 1.1.2 (10)"
+        self.Version = "Beta 1.1.2 (11)"
         print(f"[-VERSION-] {self.Version}")
         self.Zeit = "Die Zeit ist eine Illusion."
         master.title(self.Programm_Name + " " + self.Version + "                                                                          " + self.Zeit)
@@ -804,7 +804,7 @@ class Listendings:
                     print("despawn des LB_GUI zeugs")
                     self.Eintrag_verstecken_Knopp.place_forget()
                     self.Anruf_starten_LB_Knopp.place_forget()
-                    #self.per_mail_weitereiten.place_forget()   
+                    self.per_mail_weitereiten.place_forget()   
                 except:
                     print("war wohl nicht da")
 
