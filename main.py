08b2237e8e0c1ce8a3cc54ed49571062d9aec1be
D1ng5 <m.becker@beese-computer.de>
2025-02-03 13:28:15 +0100

    Beta 1.1.5
    - das Fenster ist jetzt durchsichtig, später kommt eine Einstellung hinzu um den Grad zu verändern.

--- main.py
+++ main.py
@@ -206,11 +206,12 @@ class Listendings:
         self.master = master
         self.Programm_Name = "M.U.L.M" # -> sowas nennt man übrigens ein Apronym, ist einem Akronym sehr ähnlich aber nicht gleich << Danke Du klugscheißer
         self.Programm_Name_lang = "Multifunktionaler Unternehmens-Logbuch-Manager"
-        self.Version = "Beta 1.1.4"
+        self.Version = "Beta 1.1.5"
         print(f"[-VERSION-] {self.Version}")
         self.Zeit = "Die Zeit ist eine Illusion."
         master.title(self.Programm_Name + " " + self.Version + "                                                                          " + self.Zeit)
         root.configure(resizeable=False)
+        root.attributes('-alpha',0.95)
         self.Programm_läuft = True
         self.Uhr_läuft = True
         root.protocol("WM_DELETE_WINDOW", self.bye)
